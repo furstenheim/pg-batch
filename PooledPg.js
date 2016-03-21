b4120d5ff6f706953460a7389d3895cfd2d4992e
gabriel <furstenheim@gmail.com>
2016-03-21 13:25:08 +0100

    Add pooled pg client

---
+++ PooledPg.js
@@ -0,0 +1,22 @@
+//TODO move this to separate library
+/**pooledPg is a pg client that is fetched each time it is required and returned to the pool right after, thus reducing the total amount of time out of the pool and all the problems associated with forgetting returning a client**/
+
+var PooledPg = exports.pooledPg = function (pgPool) {
+    "use strict";
+    this.pgPool = pgPool;
+};
+
+PooledPg.prototype.query = function (q, queryParams, callback) {
+    "use strict";
+    var pool = this.pgPool;
+    pool.acquire(function (err, client) {
+        if (err) {
+            console.error('error fetching postgres client from pool', err)
+            return callback(err, null);
+        }
+        client.query(q, queryParams, function (err, reply) {
+            pool.release(client);
+            callback(err, reply);
+        });
+    });
+}
